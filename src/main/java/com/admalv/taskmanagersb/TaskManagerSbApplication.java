c8a3aecaa2990986c4df45d19c044f375b63ef8f
av-malv <anandmalviya5611@gmail.com>
2023-10-12 00:28:13 +0530

    Spring boot project initialised

---
+++ src/main/java/com/admalv/taskmanagersb/TaskManagerSbApplication.java
@@ -0,0 +1,13 @@
+package com.admalv.taskmanagersb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/admalv/taskmanagersb/TaskManagerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admalv.taskmanagersb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
