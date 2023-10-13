6f060cf417f190736c7119336c6d1fe605fe7954
av-malv <anandmalviya5611@gmail.com>
2023-10-14 00:16:07 +0530

    Added notes functionality and tasks

---
+++ src/main/java/com/admalv/taskmanagersb/entities/NotesEntity.java
@@ -0,0 +1,12 @@
+package com.admalv.taskmanagersb.entities;
+
+import lombok.Data;
+
+@Data
+public class NotesEntity {
+
+    private int id;
+    private String title;
+    private String body;
+
+}
